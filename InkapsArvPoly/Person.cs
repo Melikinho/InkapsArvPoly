3116caafd9fa7af30dd7b4ca08e3638f29ad75ea
Melikinho <Jimmymelik10@gmail.com>
2022-05-18 16:58:56 +0200

    setting new ToString object method to use it in my main program

--- InkapsArvPoly/Person.cs
+++ InkapsArvPoly/Person.cs
@@ -8,42 +8,63 @@ namespace InkapsArvPoly
 {
     internal class Person
     {
-        private string firstName;
-        private string lastName;
-        private int age;
-        private double height;
-        private double weight;
+        private string _firstName;
+        private string _lastName;
+        private int _age;
+        private double _height;
+        private double _weight;
+
+        public Person(string firstName, string lastName, int age, double height, double weight)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Height = height;
+            Weight = weight;
+        }
+
 
         public string FirstName
         {
-            get { return firstName;  }
-            set{ firstName = value; }
+            get { return _firstName;  }
+            set{ _firstName = value; }
         }
         public string LastName 
         {
-        get { return lastName;} 
-            set { lastName = value; } 
+            get { return _lastName;} 
+            set { _lastName = "Melik"; } 
         }
         public int Age
         {
-            get { return age; }
-            set { age = value; }
+            get { return _age; }
+            set
+            {
+                if (value > 0)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Only greater than 0! ");
+                    throw new ArgumentException("Only greater than 0!");
+
+                }
+            }
         }
         public double Height 
         {
-            get { return height; }
-            set { height = value; }
+            get { return _height; }
+            set { _height = value; }
         }
         public double Weight
         {
-            get { return weight; }
-            set { weight = value;  }
+            get { return _weight; }
+            set { _weight = value;  }
         }
 
-
-
-
-
-
+        public override string ToString()
+        {
+            return $"First Name:\n {FirstName}\n Last Name: {LastName}\n Age: {Age}\n Height: {Height}\n Weight: {Weight}\n";
+        }
     }
 }
--- InkapsArvPoly/Program.cs
+++ InkapsArvPoly/Program.cs
@@ -1,2 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace InkapsArvPoly
 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Jimmy", "Melik", 28, 185, 85);
+            Person person1 = new Person("Zlatan", "Ibrahimovic", 39, 195, 85);
+            Console.WriteLine(person.ToString());
+            Console.WriteLine(person.ToString());
+
+        }
+    }
+}

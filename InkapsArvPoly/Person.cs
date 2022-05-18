e7b2d185859ef1fcf0d57701cabe112b7276643e
Melikinho <Jimmymelik10@gmail.com>
2022-05-18 15:36:26 +0200

    Creating a new class, Person, creating new properties with its public property, firstName, lastName, Age, height, eight

---
+++ InkapsArvPoly/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InkapsArvPoly
+{
+    internal class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private double height;
+        private double weight;
+
+        public string FirstName
+        {
+            get { return firstName;  }
+            set{ firstName = value; }
+        }
+        public string LastName 
+        {
+        get { return lastName;} 
+            set { lastName = value; } 
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public double Height 
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value;  }
+        }
+
+
+
+
+
+
+    }
+}

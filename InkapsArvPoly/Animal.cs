b8a7184537de7aad12aa18ed8ec99409113f187a
Melikinho <Jimmymelik10@gmail.com>
2022-05-20 13:37:39 +0200

    creating Animal, setting the properties, creating a constructor and the method DoSound

---
+++ InkapsArvPoly/Animal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InkapsArvPoly
+{
+    abstract class Animal
+    {
+
+        //Properties
+
+        protected string name;
+        protected int age;
+        protected double weight;
+
+        public string Name { get { return name; } set { name = value;  } }
+        public int Age { get { return age; } set { age = value; } }
+        public double Weight { get { return weight; } set { weight = value; } }
+
+        //Metod som gör ljud
+        public abstract string DoSound();
+
+        //Konstruktor
+        public Animal()
+        {
+
+        }
+
+
+    }
+
+
+
+
+}

77c4013d6e85866c4965edeadbdb00bd13d5c1f5
Melikinho <Jimmymelik10@gmail.com>
2022-05-20 16:53:15 +0200

    creating new class, Wolfman, implementing an interface  inside Wolfman. Inhertiaces from Wolf. Wolfman can also talk.

---
+++ InkapsArvPoly/Wolfman.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InkapsArvPoly
+{
+    internal class Wolfman : Wolf, IPerson
+    {
+        public string Talk()
+        {
+            Console.WriteLine("I am a WolfMan! I can talk!!!! ");
+            throw new NotImplementedException();
+        }
+    }
+}
